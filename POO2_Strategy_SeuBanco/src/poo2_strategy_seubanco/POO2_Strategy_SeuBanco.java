357708838c2017147b679523a7f2ff324d5ff513
a11421BSI210 <a11421BSI210@200.19.149.97>
2017-04-19 20:09:23 -0300

    Olá padrão Strategy

---
+++ POO2_Strategy_SeuBanco/src/poo2_strategy_seubanco/POO2_Strategy_SeuBanco.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo2_strategy_seubanco;
+
+/**
+ *
+ * @author a11421BSI210
+ */
+public class POO2_Strategy_SeuBanco {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hello Strategy Pattern.");
+        
+    }
+    
+}
