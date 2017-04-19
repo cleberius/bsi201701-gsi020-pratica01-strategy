e7f905d2111b97cf69577f0b167e64fc89fc0a8c
a11421BSI210 <a11421BSI210@200.19.149.97>
2017-04-19 20:34:22 -0300

    commmit2

---
+++ POO2_Strategy_SeuBanco/src/poo2_strategy_seubanco/Conta.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo2_strategy_seubanco;
+
+/**
+ *
+ * @author a11421BSI210
+ */
+public class Conta {
+    String numero;
+    String senha;
+    ContaBancaria conta;
+}
---
+++ POO2_Strategy_SeuBanco/src/poo2_strategy_seubanco/ContaBancaria.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo2_strategy_seubanco;
+
+/**
+ *
+ * @author a11421BSI210
+ */
+public interface ContaBancaria {
+
+        
+}
